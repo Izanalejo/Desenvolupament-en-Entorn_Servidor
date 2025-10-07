43027e4a26bd159631a45fb67f089a07dbbda7e3
Izan Alejo <izanap2006@gmail.com>
2025-10-07 13:27:54 +0200

    Exercicis PHP avanzats

--- PHP_A1/2025.10.1/Arrays/Exercicis_arraysAsociativos/Avanzados/Ex_2.php
+++ PHP_A1/2025.10.1/Arrays/Exercicis_arraysAsociativos/Avanzados/Ex_2.php
@@ -14,7 +14,11 @@ La cantidad total vendida en cada tienda.
 Procesar arrays asociativos con datos complejos y realizar operaciones de agregación.*/
 
 $ventas = [
-    ["tienda" => "Madrid", "producto" => "Laptop", "cantidad" => 5],
+    ["tienda" => "Madrid", 
+    "producto" => "Laptop", 
+    "cantidad" => 5,
+    "precio" => 50 ],
+
     ["tienda" => "Madrid", "producto" => "Tablet", "cantidad" => 3],
     ["tienda" => "Barcelona", "producto" => "Laptop", "cantidad" => 2],
     ["tienda" => "Madrid", "producto" => "Smartphone", "cantidad" => 8],
@@ -26,11 +30,59 @@ function agruparPorProducto($ventas): array {
 
     foreach($ventas as $venta){
         $producto = $venta['producto'];
-        $agrupadosPorProducto[$producto][] = $venta; 
+    
+        $agrupadosPorProducto[$producto][] = $venta['cantidad']; 
     }
     return $agrupadosPorProducto;
 }
+function imprimirProductosTotales($arrayAgrupado){
+    echo "<h2>Cantidad vendida de cada producto</h2>";
 
+    foreach ($arrayAgrupado as $producto => $arrayCantidad) {
+        echo "<pre>";
+        echo "<h3>$producto</h3> Quantitat:" . array_sum($arrayCantidad);
+    }
+
+}
+
+function agruparPorTienda($ventas): array {
+    $agrupadosPorTienda = [];
+
+    foreach($ventas as $venta){
+        $tienda = $venta['tienda'];
+
+        $agrupadosPorTienda[$tienda][] = $venta['cantidad'];
+    }
+   return $agrupadosPorTienda;
+}
+
+function imprimirPorTienda($arrayAgrupado){
+    echo "<h2>Cantidad vendida por tienda</h2>";
+
+    foreach ($arrayAgrupado as $tienda => $arrayCantidad) {
+        echo "<pre>";
+        echo "<h3>$tienda </h3> Quantitat:" . array_sum($arrayCantidad);
+    }
+
+}
+//main
+
+$agrupadosPorProductos = []; //Creo un array vacío
+
+$agrupadosPorProductos = agruparPorProducto($ventas); // Aqui añado información al array creado a partir de la función agruparPorProducto
+
+imprimirProductosTotales($agrupadosPorProductos); 
+
+$agrupadosPorTienda = [];
+
+$agrupadosPorTienda = agruparPorTienda($ventas);
+
+imprimirPorTienda($agrupadosPorTienda);
+
+
+
+//////////////////////////////////////////////////////////////////////////
+/* 
 function agruparPorTienda($ventas): array {
     $agrupadosPorTienda = [];
     foreach($ventas as $venta){
@@ -63,11 +115,17 @@ function imprimirProductosVendidos($agrupados){
          echo "<h3>$producto:  $total Unidades</h3>";
     }
 }
+ */
 
-$agrupados = agruparPorProducto($ventas);
-imprimirProductosVendidos($agrupados);
+//main
+/*  $agrupadosPorProducto = agruparPorProducto($ventas);
+/* imprimirProductosVendidos($agrupadosPorProducto);
 
 $agrupadosPorTienda = agruparPorTienda($ventas);
-imprimirProductosPorTienda($agrupadosPorTienda);
+imprimirProductosPorTienda($agrupadosPorTienda); 
+ 
+        echo "<pre>";
+       print_r($agrupadosPorProducto); */
+    
 
 ?>
