394cd69085ae27f31124566586eace1b2dee10b5
Izan Alejo <izanap2006@gmail.com>
2025-10-22 13:53:24 +0200

    Ejercicios formularios 402-407

---
+++ PHP_A1/2025.10.22/Practica_integrativa/includes/navbar.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a href="index.php"  class="nav-link active" aria-current="page" >Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a href="about.php"  class="nav-link" href="#">Acerca de</a>
+        </li>
+        <li class="nav-item">
+          <a href="services.php"  class="nav-link" href="#">Servicios</a>
+        </li>
+        <li class="nav-item">
+          <a href="contact.php"  class="nav-link" href="#">Contactanos</a>
+        </li>
+        <li class="nav-item">
+          <a href="../cookies_sesiones/index.php"  class="nav-link" href="#">Cookies</a>
+        </li>
+        <li class="nav-item">
+          <a href="formularios/index.php" class="nav-link">Formularios</a>
+        </li>
+       
+      </ul>
+      
+      </form>
+    </div>
+  </div>
+</nav>
