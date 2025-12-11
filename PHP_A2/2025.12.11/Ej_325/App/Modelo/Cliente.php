fb13da030e947ae6d519d3117422ec15063ab979
Izan Alejo <izanap2006@gmail.com>
2025-12-11 14:55:10 +0100

    Cliente mejorado

--- PHP_A2/2025.12.11/Ej_325/App/Modelo/Cliente.php
+++ PHP_A2/2025.12.11/Ej_325/App/Modelo/Cliente.php
@@ -1,13 +1,17 @@
 <?php
+
 namespace App\Modelo;
-class Cliente{
+
+class Cliente
+{
     private string $nombre;
     private int $numero;
     private array $soportesAlquilados;
     private int $numAlquileres;
     private int $maxAlquilerConcurrente;
 
-    public function __construct(string $nombre, int $numero, int $maxAlquilerConcurrente = 3){
+    public function __construct(string $nombre, int $numero, int $maxAlquilerConcurrente = 3)
+    {
         $this->nombre = $nombre;
         $this->numero = $numero;
         $this->maxAlquilerConcurrente = $maxAlquilerConcurrente;
@@ -17,19 +21,20 @@ class Cliente{
     public function getNumero()
     {
         return $this->numero;
-    } 
+    }
     public function setNumero($numero)
     {
         $this->numero = $numero;
 
         return $this;
     }
-     public function getNumSoportesAlquilados()
-     {
+    public function getNumSoportesAlquilados()
+    {
         return $this->numAlquileres;
-     }
+    }
 
-    public function muestraResumen(): void{
+    public function muestraResumen(): void
+    {
         $length = count($this->soportesAlquilados);
         $html = <<< HTML
             <hr>Nombre: {$this->nombre} <br>
@@ -39,36 +44,31 @@ class Cliente{
         echo $html;
     }
 
-    public function alquilar(Soporte $s): bool{
-        if($this->tieneAlquilado($s)){
+    public function alquilar(Soporte $s): bool
+    {
+        if ($this->tieneAlquilado($s)) {
             echo "El soporte ya está alquilado por el cliente.<br>";
             return false;
-        } else if($this->numAlquileres >= $this->maxAlquilerConcurrente){ {
-            echo "<p style='color:red;'>El cliente ha alcanzado el máximo de alquileres concurrentes.</p><br>";
-            return false;
+        } else if ($this->numAlquileres >= $this->maxAlquilerConcurrente) { {
+                echo "<p style='color:red;'>El cliente ha alcanzado el máximo de alquileres concurrentes.</p><br>";
+                return false;
+            }
         }
-        }else if($this->numAlquileres < $this->maxAlquilerConcurrente){
-                $this->numAlquileres++;
-                $this->soportesAlquilados[] = $s;
-                echo "Soporte alquilado numero: {$this->numAlquileres} correctamente.<br>";
-                return true;
-            } 
-        return false;
+        $this->numAlquileres++;
+        $this->soportesAlquilados[] = $s;
+        echo "Soporte alquilado numero: {$this->numAlquileres} correctamente.<br>";
+        return true;
     }
-    public function tieneAlquilado(Soporte $s): bool{
-        foreach($this->soportesAlquilados as $soporte){
-            if($soporte === $s){
-                if($s instanceof CintaVideo || $s instanceof Dvd || $s instanceof Juego){
-                    echo "<p style='color:green;'>El soporte es de tipo: ". get_class($s) . "</p><br>";
+    public function tieneAlquilado(Soporte $s): bool
+    {
+        foreach ($this->soportesAlquilados as $soporte) {
+            if ($soporte === $s) {
+                if ($s instanceof CintaVideo || $s instanceof Dvd || $s instanceof Juego) {
+                    echo "<p style='color:green;'>El soporte es de tipo: " . get_class($s) . "</p><br>";
                     return true;
                 }
             }
         }
         return false;
     }
-
 }
-
-
-
-?>
